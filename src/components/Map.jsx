2e77b3e82693611b8fb83d0077f1e70de6ae2ff9
Reyan Hussain <clan6229@gmail.com>
2025-11-19 20:49:17 +0530

    commit1

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,19 @@
 import { useEffect, useState } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+
+
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"; //Integration Of Maps
 import "./Map.css";
 
