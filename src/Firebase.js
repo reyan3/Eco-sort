fa2241ba00292a25cd482bdd6d23edd0b34c986a
Reyan Hussain <clan6229@gmail.com>
2026-05-02 22:22:06 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -7,9 +7,9 @@ import { getFirestore } from "firebase/firestore"; // 1. Import Firestore
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebase_api = import.meta.env.FIREBASE_API
+const FIREAPI = import.meta.env.FIRE_API
 const firebaseConfig = {
-  apiKey: firebase_api,
+  apiKey: FIREAPI,
   authDomain: "ecosort-1e92b.firebaseapp.com",
   projectId: "ecosort-1e92b",
   storageBucket: "ecosort-1e92b.firebasestorage.app",
